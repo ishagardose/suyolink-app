e61732a5fe9659026888849de477ac07a018d71b
ishagardose <ishagardose@gmail.com>
2026-09-10 13:29:38 +0800

    Initial commit with SuyoLink app

---
+++ app/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: '500',
+    color: '#000000',
+  },
+});
